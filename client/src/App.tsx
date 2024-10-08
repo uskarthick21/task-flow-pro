35401b81cec4e96d29ed3499af1db37ead3cb84a
uskarthick21 <uskarthick21@gmail.com>
2024-10-08 11:58:57 +1100

    Front end layout setup with router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,20 @@
-function App() {
+import React from 'react'
+
+import Layout from './layouts/Layout'
+import { Navigate, Route, Routes } from 'react-router-dom'
+
+
+const App = () => {
   return (
-    <div className="App bg-sky-500">
-    APP
-    </div>
-  );
+  
+      <Routes>
+        <Route path="/" element={<Layout><p>Home Page</p></Layout>} />
+        <Route path="/search" element={<>Search Page</>} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+
+  )
 }
 
-export default App;
+export default App
+
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+const Header = () => {
+	return (
+		<div className="bg-blue-800 py-6">
+			<div className="container mx-auto flex justify-between">
+				<span className="text-3xl text-white font-bold tracking-tight">
+					<Link to="/">Task Management Pro</Link>
+				</span>
+				<span className="flex space-x-2">
+					<Link
+						className="flex bg-white items-center text-blue-600 px-3 font-bold hover:bg-gray-100 "
+						to="/sign-in"
+					>
+						Sign Up
+					</Link>
+				</span>
+			</div>
+		</div>
+	);
+};
+export default Header;
