ea72af17c93eb05bc76fdf8670919962c67d5874
uskarthick21 <uskarthick21@gmail.com>
2024-10-12 08:57:50 +1100

    no base url production issue fix and path added in backend

--- client/src/config/apiClient.ts
+++ client/src/config/apiClient.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 // You need to add prefix REACT_APP_ to the variable name:
+// baseURL: process.env.REACT_APP_TASK_FLOW_APP_BFF_URL || '' , if there is no base url in production then just same server for all request. thats why given like REACT_APP_TASK_FLOW_APP_BFF_URL  || ''
 const options = {
-    baseURL: process.env.REACT_APP_TASK_FLOW_APP_BFF_URL,
+    baseURL: process.env.REACT_APP_TASK_FLOW_APP_BFF_URL || '',
     withCredentials: true,
 };
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,7 @@ import connectDB from './config/db';
 import errorHandler from './middleware/errorHandlers';
 import { OK } from './constants/http';
 import authRoutes from './routes/auth.routes';
+import path from "path";
 
 const app = express();
 app.use(express.json())
@@ -22,17 +23,17 @@ app.use(
 // app.get("/", async (req, res)) if we call async, then entire function would return promise rejection. 
 // we have to wrap everything try and catch block.
 
-app.get("/", (req, res) => {
-    res.status(OK).json({
-        status: "healthy",
-    })
-})
+app.use(express.static(path.join(__dirname, "../../client/build")));
 
 app.use("/auth", authRoutes);
 
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend/build/index.html"));
+});
+
 app.use(errorHandler);
 
-app.listen(4001, async() => {
+app.listen(PORT, async() => {
     console.log(`server is running on port ${PORT} in ${NODE_ENV} enviroment`);
     await connectDB();
 })
