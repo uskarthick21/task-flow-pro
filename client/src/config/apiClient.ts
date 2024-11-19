71b44bc1909990cb40e224a1349f9fa8c3c4a7c1
uskarthick21 <uskarthick21@gmail.com>
2024-11-19 21:08:50 +1100

    investgating production issue

--- client/src/config/apiClient.ts
+++ client/src/config/apiClient.ts
@@ -1,6 +1,50 @@
+// import axios from 'axios';
+// import { queryClient } from '..';
+// import { navigate } from './navigation';
+
+// // You need to add prefix REACT_APP_ to the variable name:
+// const options = {
+//     baseURL: process.env.REACT_APP_TASK_FLOW_APP_BFF_URL,
+//     withCredentials: true,
+// };
+
+// const TokenRefreshClient = axios.create(options);
+// TokenRefreshClient.interceptors.response.use((response) => response.data);
+
+// const API = axios.create(options)
+
+// API.interceptors.response.use(
+//     (response) => response.data,
+//     async (error) => {
+//         //console.log("Error:", JSON.stringify(error, null, 4));
+
+//        const {config, response} = error;
+//         const {status, data} = response || {};
+
+//         // try to refresh the access token behind the scenes
+//         if(status === 401 && data?.errorCode === "InvalidAccessToken") {
+//             try {
+//                 await TokenRefreshClient.get("/auth/refresh");
+//                 // after get accessToken from above then retry the original request from the error config. That what below code doing.
+//                 return TokenRefreshClient(config);
+//             } catch (error) {
+//                 queryClient.clear();
+//                 localStorage.removeItem('isLoggedIn');
+//                 navigate("/login", {
+//                     state: {
+//                         redirectUrl: window.location.pathname,
+//                     }
+//                 })
+//             }
+//         }
+
+//         return Promise.reject({status, ...data})
+//     }
+// )
+
+// export default API
+
 import axios from 'axios';
-import { queryClient } from '..';
-import { navigate } from './navigation';
 
 // You need to add prefix REACT_APP_ to the variable name:
 const options = {
@@ -8,36 +52,12 @@ const options = {
     withCredentials: true,
 };
 
-const TokenRefreshClient = axios.create(options);
-TokenRefreshClient.interceptors.response.use((response) => response.data);
-
 const API = axios.create(options)
 
 API.interceptors.response.use(
     (response) => response.data,
-    async (error) => {
-        //console.log("Error:", JSON.stringify(error, null, 4));
-
-       const {config, response} = error;
-        const {status, data} = response || {};
-
-        // try to refresh the access token behind the scenes
-        if(status === 401 && data?.errorCode === "InvalidAccessToken") {
-            try {
-                await TokenRefreshClient.get("/auth/refresh");
-                // after get accessToken from above then retry the original request from the error config. That what below code doing.
-                return TokenRefreshClient(config);
-            } catch (error) {
-                queryClient.clear();
-                localStorage.removeItem('isLoggedIn');
-                navigate("/login", {
-                    state: {
-                        redirectUrl: window.location.pathname,
-                    }
-                })
-            }
-        }
-
+    (error) => {
+        const {status, data} = error.response;
         return Promise.reject({status, ...data})
     }
 )
--- server/src/utils/cookies.ts
+++ server/src/utils/cookies.ts
@@ -3,12 +3,12 @@ import { fifteenMinutesFromNow, thirtyDaysFromNow } from "./date";
 import { setAuthCookiesParams } from "./dataTypes";
 
 export const REFRESH_PATH = "/auth/refresh";
-const secure = process.env.NODE_ENV !== "development";
 
 const defaults: CookieOptions = {
   sameSite: "strict",
   httpOnly: true,
-  secure,
+  // When the secure option is set to true, the cookie will only be sent over HTTPS connections. This means the cookie cannot be transmitted over unencrypted HTTP, which helps protect the cookie from being intercepted by attackers during transmission.
+  secure: true,
 };
 
 export const getAccessTokenCookieOptions = (): CookieOptions => ({
@@ -23,14 +23,12 @@ export const getRefreshTokenCookieOptions = (): CookieOptions => ({
   path: REFRESH_PATH, // Set path to limit exposure to the refresh endpoint
 });
 
-// Function to set both access and refresh token cookies
 export const setAuthCookies = ({ res, accessToken, refreshToken }: setAuthCookiesParams) =>
   res
     .cookie("accessToken", accessToken, getAccessTokenCookieOptions())
     .cookie("refreshToken", refreshToken, getRefreshTokenCookieOptions());
 
-// Function to clear both cookies with matching options
-export const clearAuthCookies = (res: Response) =>
-  res
-    .clearCookie("accessToken", { path: "/", maxAge: 0 }) // Ensure path matches the one used to set
-    .clearCookie("refreshToken", { path: REFRESH_PATH }); // Use sam
+    export const clearAuthCookies = (res: Response) =>
+      res
+        .clearCookie("accessToken")
+        .clearCookie("refreshToken", { path: REFRESH_PATH });
